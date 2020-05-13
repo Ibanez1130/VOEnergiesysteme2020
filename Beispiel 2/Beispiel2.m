d551d47f9839280f505c83c4e33127c444c8114d
Konstantin Kobel <konstantin.kobel13@gmail.com>
2020-05-13 18:46:10 +0200

    Aufgabe 2.1 begonnen

--- Beispiel 2/Beispiel2.m
+++ Beispiel 2/Beispiel2.m
@@ -6,6 +6,7 @@ clc                                 % Leert Command Window
 % Einlesen der Daten
 load ('./Angabe/Strahlung.mat');
 load ('./Angabe/time.mat');
+load ('./Angabe/Temperatur.mat');
 addpath(genpath('Angabe')); % Hinzufügen des Angabe Ordners zum aktuellen Pfad, damit wir die Funktionen darin ausführen können.
 
 % Ausgabe der Tabellen-Header von Strahlung.mat
@@ -20,6 +21,9 @@ sBreitengrad = 48.2; % Breitengrad von Wien
 pvWirkungsgrad = 0.17; % Modulwirkungsgrad
 pvVerluste = 0.8; % Sonstige Verluste (Reflexion, Temperatur, Wechselrichter, etc.)
 albedo = 0.2; % lt. Blabensteiner2011 - bei unbekannter Umgebung
+gSTC = 1000; % Strahlung laut Standard Test Condition - in W/m^2
+TmodSTC = 25; % Temperaturänderung der PV-Anlage laut Standard Test Conditions.
+ct = 0.026; % Beschreibt wie stark die PV-Anlage durch die Strahlung erhitzt wird.
 
 %% Berechnung des Sonnenazimutal- und Höhenwinkels mittels der zur Verfügung gestellten Funktion SonnenstandTST
 [sAzimut,sHoehenwinkel] = SonnenstandTST(sLaengengrad,sBreitengrad,time);
@@ -28,81 +32,24 @@ albedo = 0.2; % lt. Blabensteiner2011 - bei unbekannter Umgebung
 % Moduleinfallswinkel bei einer Südausrichtung von 180°
 pvModuleinfallswinkel = acosd(-cosd(sHoehenwinkel).*sind(pvHoehenwinkel).*cosd(sAzimut - pvAzimut - 180)+sind(sHoehenwinkel).*cosd(pvHoehenwinkel));
 
-
 %% Funktion: Eges = Jahreserzeugung(...) aufrufen. Funktion muss zuvor erstellt werden.
-Eges = Jahreserzeugung(pvHoehenwinkel, pvGroesse, pvWirkungsgrad, pvVerluste, pvModuleinfallswinkel, sHoehenwinkel, Strahlung);
+[Eges,EgesT] = Jahreserzeugung(pvHoehenwinkel, pvGroesse, pvWirkungsgrad, pvVerluste, pvModuleinfallswinkel, sHoehenwinkel, Strahlung, gSTC, TmodSTC, ct, Temperatur);
+EgesT(isnan(EgesT))=0;
 
-%Berechnung der Vollaststunden
-T = sum(Eges)/(pvGroesse.*1000);
-
-%% Darstellung mittels plot(Eges)
-figure('Name', 'Gesamtertrag (1.1.b)', 'NumberTitle', 'Off')
-plot(Eges)
-xlabel("Zeit in Vierteltunden")
-ylabel("Gesamtertrag in Wh/15min")
-axis([0 35040 0 inf])
-
-%% Aufgabe 1.2.a
-figure('Name', 'Leistungsdauerlinie (1.2.a)', 'NumberTitle', 'Off')
-Psorted = sort(Eges, 'descend').*15;
-plot(Psorted)
-xlabel("Zeit in Viertelstunden")
-ylabel("Leistung in W")
-axis([0 35040 0 inf])
-
-%% Aufgabe 1.2.b und 1.2.c
-
-%Aufgabe 1.2.b
+% Plot der Differenz zwischen Eges (ideal) und EgesT (Berücksichtigung der
+% Temperatur)
+% plot(abs(Eges-EgesT))
+% Aufsummieren der jeweiligen Monats-Summen
 Emon = zeros(1,12);
+EmonT = zeros(1,12);
 for monat=1:12
     Emon(monat) = sum(Eges(time.Monat == monat));
-end   
-figure('Name', 'Monatliche Erträge (1.2.b)', 'NumberTitle', 'Off')
-bar(Emon)
+    EmonT(monat) = sum(EgesT(time.Monat == monat));
+end
+figure('Name', 'Monatliche Erträge (2.1.b)', 'NumberTitle', 'Off')
+bar(1:12,[Emon;EmonT]);
+legend('ideal','temperaturabhängig')
 xlabel("Monat")
 ylabel("Ertrag in Wh/Monat")
-close all
 
-% Aufgabe 1.2.c
-Etag = zeros(1,365);
-for tag=1:365
-    Etag(tag) = sum(Eges(time.Tag == tag));
-end
-% 5 Tage mit der maximalen PV-Erzeugung:
-[maxPV,I] = maxk(Etag,5);
-maxDates = I+datetime(Strahlung.Year(1),1,0);
-
-% 5 Tage mit der minimalen PV-Erzeugung:
-[minPV,J] = mink(Etag,5);
-minDates = J+datetime(Strahlung.Year(1),1,0);
-
-%% Diagramm f�r Strahlungsanteile: plotStrahlungsanteile(...)
-plotStrahlungsanteile(pvAzimut, pvHoehenwinkel, sLaengengrad, sBreitengrad, Strahlung, time)
-
-%% Aufgabe 1.2.e
-% Juni
-% Die Daten aus Eges auf den Monat Juni beschränken
-Ejuni = Eges(time.Monat == 6);
-% Zusammenfassen der Viertelstunden-Werte zu jeweils einem Stundenwert.
-% "Jeder Stundenwert besteht aus der Summe von vier Viertelstundenwerten."
-EJuniStunden = sum(reshape(Ejuni,4,720));
-% Umformen der 1x720 Matrix in eine 24 x 30 Matrix.
-EJuniTage = reshape(EJuniStunden,24,30);
-
-boxplot(EJuniTage)
-xlabel('Tage (Juni 2005)')
-ylabel('Ertrag in Watt')
-title('Ertrag pro Stunde - Juni')
-
-% Dezember
-% Die Schritte für Dezember sind die selben wie für Juni.
-% Ein Unterschied ist, dass Dezember 31 Tage hat und daher die Anzahl der
-% Stunden und Tage unterschiedlich sind.
-Edezember = Eges(time.Monat == 12);
-EDezemberStunden = sum(reshape(Edezember,4,744));
-EDezemberTage = reshape(EDezemberStunden,24,31);
-
-boxplot(EJuniTage)
-xlabel('Tage (Dezember 2005)')
-ylabel('Ertrag in Watt')
-title('Ertrag pro Stunde - Dezember')
+% TODO: Stellen Sie dazu die durchschnittlichen stündlichen Werte gegenüber.
--- Beispiel 2/Jahreserzeugung.m
+++ Beispiel 2/Jahreserzeugung.m
@@ -1,5 +1,5 @@
 %Berechnet die viertelstündlichen Erträge der Anlage
-function Eges = Jahreserzeugung (pvHoehenwinkel, pvGroesse, pvWirkungsgrad, pvVerluste, pvModuleinfallswinkel, sHoehenwinkel, Strahlung)
+function [Eges,EgesT] = Jahreserzeugung (pvHoehenwinkel, pvGroesse, pvWirkungsgrad, pvVerluste, pvModuleinfallswinkel, sHoehenwinkel, Strahlung, gSTC, TmodSTC, ct, Temperatur)
 
 %Flaeche des PV-Moduls
 pvFlaeche = pvGroesse./pvWirkungsgrad;
@@ -17,8 +17,13 @@ DiffusGen = Strahlung.DiffusHoriz.*(1+cosd(pvHoehenwinkel))./2;
 GesGen = DirectGen + ReflectedGen + DiffusGen;
 GesGen(sHoehenwinkel < 5) = 0;  %Strahlung bei Hoehenwinkel unter 5° wird nicht berücksichtigt
 
+Tmod = repelem(Temperatur,4) + ct.*GesGen;
+T = Tmod - TmodSTC;
+TWirkungsgrad=1-0.017162*log(GesGen./gSTC)-0.040289*log(GesGen./gSTC).^2-0.004681.*log(GesGen./gSTC).^3+0.000148.*log(GesGen./gSTC).^4+0.000169.*log(GesGen./gSTC).^5+0.000005.*T.^2;
+
 %Ertrag in 15min Intervallen
 Eges = GesGen.*0.25.*pvFlaeche.*pvWirkungsgrad.*pvVerluste;
+% Ertrag mit Einfluss der Temperatur
+EgesT = GesGen.*0.25.*pvFlaeche.*pvWirkungsgrad.*pvVerluste.*TWirkungsgrad;
 
 end
-
